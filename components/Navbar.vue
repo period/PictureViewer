fcd2401b72c666eedf14ef5618de0b55c62eaac1
Thomas <chearful@chearfulcraft.org>
2020-05-05 17:49:05 +0100

    Home page + navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+  
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="/">pics.thomas.gg</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+          <b-nav-item to="/albums">Albums</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-button to="/search">Search</b-button>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
