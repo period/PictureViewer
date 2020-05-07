dbbaee50d6101718b0073db2af52569113e16cce
Thomas <chearful@chearfulcraft.org>
2020-05-07 23:24:39 +0100

    Add rel=nofollow to info link

--- pages/info.vue
+++ pages/info.vue
@@ -3,7 +3,7 @@
     <h1>Welcome to pics.thomas.gg!</h1>
     <p>This is my corner of the internet where I keep all of my photos of aircraft in the sky and on the ground. All photos are organised by their registration, which allows for additional metadata to be attached.</p>
     <h3>Credits</h3>
-    <p>Most of the additional metadata relating to an aircraft is pulled from <a href="https://www.opensky-network.org">The OpenSky Network</a>.</p>
+    <p>Most of the additional metadata relating to an aircraft is pulled from <a rel="nofollow" href="https://www.opensky-network.org">The OpenSky Network</a>.</p>
     <h3>Contact</h3>
     <p>I can be reached by email by replacing the first '.' of this site's address with an @.</p>
     <h3>A word on copyright</h3>
