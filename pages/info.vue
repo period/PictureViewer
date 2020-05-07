5ad6e53d9299efca5d60d34d8f775a0b2a63753e
Thomas <chearful@chearfulcraft.org>
2020-05-07 23:18:24 +0100

    Add info page

---
+++ pages/info.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Welcome to pics.thomas.gg!</h1>
+    <p>This is my corner of the internet where I keep all of my photos of aircraft in the sky and on the ground. All photos are organised by their registration, which allows for additional metadata to be attached.</p>
+    <h3>Credits</h3>
+    <p>Most of the additional metadata relating to an aircraft is pulled from <a href="https://www.opensky-network.org">The OpenSky Network</a>.</p>
+    <h3>Contact</h3>
+    <p>I can be reached by email by replacing the first '.' of this site's address with an @.</p>
+    <h3>A word on copyright</h3>
+    <p>Unless otherwise stated, all photos are owned by myself. Please don't use my photos elsewhere without my permission!</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Info"
+};
+</script>
