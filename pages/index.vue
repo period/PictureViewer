018634a132ea3b7cac127cfe370af5dc3a452398
Thomas <chearful@chearfulcraft.org>
2021-01-14 16:49:04 +0000

    Featured photo on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <h1>Welcome to pics.thomas.gg!</h1>
-      <!---
-      <b-jumbotron
-        header="pics.thomas.gg"
-        lead="A collection of pictures of planes both low and high that I take whilst out and about. Nothing special, really."
-      >
-      </b-jumbotron>
-      -->
+<div>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.6.6/glider.min.css" integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg==" crossorigin="anonymous" />
+  <div class="container-fluid">
+    <div class="background-header-holder">
+      <div class="glider-contain">
+        <div class="glider">
+          <div class="background-header-image">
+            <img src="https://pics.thomas.gg/storage/full/77de94b1-dfb2-4dc0-8a9b-d5d1bb74dfeb.jpg">
+            <div class="background-header-text">
+              <h1>Welcome to pics.thomas.gg!</h1>
+            </div>
+          </div>
+        </div>
+      </div>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <h5>Featured photos</h5>
-      <b-carousel
-        v-model="slide"
-        controls
-        indicators
-        fade
-      >
-        <b-carousel-slide
-          v-for="photo in featured"
-          :key="photo.uuid"
-          :caption="photo.registration"
-          :text="photo.caption"
-          :img-src="'https://pics.thomas.gg/storage/full/'+photo.uuid+'.jpg'"
-        />
-      </b-carousel>
     </div>
   </div>
+</div>
 </template>
-
+<style scoped>
+.background-header-holder {
+  margin-left: -15px;
+  margin-right: -15px;
+}
+.background-header-image {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.background-header-text {
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
 <script>
 export default {
   name: "Home",
@@ -39,6 +47,7 @@ export default {
     }
   },
   async mounted() {
+    new Glider(document.querySelector('.glider'));
     await this.$axios
         .$get("https://pics.thomas.gg/api/featured", {})
         .then(res => {
