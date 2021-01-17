952b2c4b3c196f4063849695a97cb38f95ff9656
Thomas <chearful@chearfulcraft.org>
2021-01-17 13:58:23 +0000

    Collapse navbar no jq

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -54,13 +54,60 @@
 @keyframes fadeOut {
    0% {background-color: rgba(79,102,115,1);}
    100% {background-color:rgba(79,102,115,0);}
-} 
+}
 </style>
 <script>
 export default {
   name: "Navbar",
   mounted() {
     window.addEventListener("scroll", this.onScroll);
+
+    // Collapsible navbar without jquery taken from https://codepen.io/jasonbutz/pen/yPodOE
+    let collapseElements = document.querySelectorAll('[data-toggle="collapse"]');
+    const CLASS_SHOW = 'show';
+    const CLASS_COLLAPSE = 'collapse';
+    const CLASS_COLLAPSING = 'collapsing';
+    const CLASS_COLLAPSED = 'collapsed';
+    const ANIMATION_TIME = 350; // 0.35s
+
+    const handleCollapseElementClick = (e => {
+        let el = e.currentTarget;
+        let collapseTargetId = el.dataset.target || el.href || null;
+        if (collapseTargetId) {
+            let targetEl = document.querySelector(collapseTargetId);
+            let isShown = targetEl.classList.contains(CLASS_SHOW) || targetEl.classList.contains(CLASS_COLLAPSING);
+            if(!isShown) {
+                targetEl.classList.remove(CLASS_COLLAPSE);
+                targetEl.classList.add(CLASS_COLLAPSING);
+                targetEl.style.height = 0
+                targetEl.classList.remove(CLASS_COLLAPSED);
+                setTimeout(() => {
+                    targetEl.classList.remove(CLASS_COLLAPSING);
+                    targetEl.classList.add(CLASS_COLLAPSE, CLASS_SHOW);
+                    targetEl.style.height = '';
+                }, ANIMATION_TIME)
+                targetEl.style.height = targetEl.scrollHeight + 'px';
+                /* treat navbar as being scrolled down/non home page so that it gets given a background so easier to see */
+                this.noAnimate = true;
+                this.fadeIn = false;
+                this.fadeOut = false;
+            } else {
+                targetEl.style.height = `${targetEl.getBoundingClientRect().height}px`
+                targetEl.offsetHeight; // force reflow
+                targetEl.classList.add(CLASS_COLLAPSING);
+                targetEl.classList.remove(CLASS_COLLAPSE, CLASS_SHOW);
+                targetEl.style.height = '';
+                setTimeout(() => {
+                    targetEl.classList.remove(CLASS_COLLAPSING);
+                    targetEl.classList.add(CLASS_COLLAPSE);
+                }, ANIMATION_TIME)
+            }
+        }
+    })
+
+    collapseElements.forEach((el) => {
+        el.addEventListener('click', handleCollapseElementClick)
+    })
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.onScroll);
